b3bb035fdd37571fafa319735da5843588a194e5
Igor <pingwin91@gmail.com>
2022-12-21 12:08:18 +0300

    task 26 is closed

--- var01/26.py
+++ var01/26.py
@@ -9,27 +9,41 @@ for i in f:
     files.append(int(i))
 files.sort()
 
+
 # Находим максимальный обхем файлов, которые точно попадут в архив
 # удаляем последний поместившийся элемент, чтобы подобрать на последнюю вакансию файл наибольшего объема
+answer_mass = []
 max_val = 0
 for i in range(n):
-    x = files[i]
     if max_val + files[i] <= value:
         max_val += files[i]
     else:
         max_val -= files[i - 1]
         break
+    answer_mass.append(files[i])
+
+# Реверсируем оба массива
+answer_mass.reverse()
+files.reverse()
 
 # Подбираем наибольший файл на последнюю вакансию в архив
-index = n - 1
+index = 0
 count = 0
 while True:
-    x = files[index]
     if max_val + files[index] <= value:
         max_val += files[index]
+        answer_mass.append(files[index])
+        i = 0
+        index += 1
+        while max_val < value:
+            if max_val - answer_mass[i] + files[index] <= value:
+                max_val = max_val - answer_mass[i] + files[index]
+                answer_mass[i] = files[index]
+                i += 1
+            index += 1
         break
     else:
-        index -= 1
+        index += 1
         count += 1
 
 print(max_val, count)
